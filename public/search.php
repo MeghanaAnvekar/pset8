7c728d29a1eecf6745bc062536abe8d9870c7d4d
Meghana Anvekar <maanvekar@gmail.com>
2016-12-23 12:22:52 +0000

    search restored , lost due to pset8.zip reload

--- public/search.php
+++ public/search.php
@@ -6,6 +6,16 @@
     $places = [];
 
     // TODO: search database for places matching $_GET["geo"], store in $places
+    $p = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name,admin_name1) AGAINST (?)", $_GET["geo"]);
+    
+    foreach($p as $row)
+    {
+        $places[]=[
+            "postal code" => $row["postal_code"],
+            "city" => $row["postal_code"],
+            "state" => $row["admin_name1"]
+            ];
+    }
 
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
